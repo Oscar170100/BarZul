d5469488e88298821ba4035ab9026a1cc34fa0e0
Marylu95 <marissa.castillo@estudiantxs.uacm.edu.mx>
2026-04-25 23:28:18 -0600

    Creacion de la pantalla de ticket

---
+++ src/main/java/org/uacm/barzul/bar/TicketController.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package org.uacm.barzul.bar;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author maris
+ */
+public class TicketController implements Initializable {
+
+    @FXML
+    private TableView<?> tbTicket;
+    @FXML
+    private TableColumn<?, ?> celCantidad;
+    @FXML
+    private TableColumn<?, ?> celDescripcion;
+    @FXML
+    private TableColumn<?, ?> celPrecio;
+    @FXML
+    private Text lblNumtiket;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
